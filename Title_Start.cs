c7cf0b3b8fe89eff13dda292f6aa935a6e2d5ab3
Lee gwonwoo <229861@naver.com>
2024-06-19 22:19:06 +0900

    Initial commit

---
+++ Title_Start.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title_Start : MonoBehaviour
+{
+     public void StarBtn() {
+        SceneManager.LoadScene("project02");
+     }
+   
+}
